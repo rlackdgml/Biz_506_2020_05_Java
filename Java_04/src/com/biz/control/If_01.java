45e9e06fc1cf9fe05219a9e83bfa81cbcb56f443
rlackdgml <dhdlqkqh2003@naver.com>
2020-05-21 17:27:55 +0900

    if 명령연습

---
+++ Java_04/src/com/biz/control/If_01.java
@@ -0,0 +1,28 @@
+package com.biz.control;
+
+public class If_01 {
+	
+	public static void main(String[] args) {
+		boolean bVar = true;
+		if(bVar) {
+			System.out.println("bVar는 treu");
+			}
+		
+		
+		if(bVar) {
+			System.out.println("32는 짝수");
+		}
+		/*
+		 *  if() 명령문을 사용할떄
+		 *  boolean 형 변수를 만들어서
+		 *  연산결과를 변수에 담고 
+		 *  변수의 true false 판단하여 어떤 처리를 하는 경우 보다는
+		 *  직접 if() 명령문에 연산식을 집어넣어 
+		 *  처리하는 경우가 더 많다.
+		 */
+		
+			System.out.println("32는 짝수");
+		}
+	}
+
+
