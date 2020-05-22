742f546e144dad3d9f696285075a00048a82b19d
rlackdgml <dhdlqkqh2003@naver.com>
2020-05-22 17:40:38 +0900

    for 명령문

---
+++ Java_05/src/com/biz/hello/Var_01.java
@@ -0,0 +1,98 @@
+package com.biz.hello;
+
+public class Var_01 {
+	
+	public static void main(String[] args) {
+		
+		int num1 = 2;
+		int num2 = 0;
+		System.out.println("===========================================");
+		
+		System.out.println(num1 + "x" + ++num2 +"=" +(num1 * num2));
+		System.out.println(num1 + "x" + ++num2 +"=" +(num1 * num2));
+		System.out.println(num1 + "x" + ++num2 +"=" +(num1 * num2));
+		System.out.println(num1 + "x" + ++num2 +"=" +(num1 * num2));
+		System.out.println(num1 + "x" + ++num2 +"=" +(num1 * num2));
+		System.out.println(num1 + "x" + ++num2 +"=" +(num1 * num2));
+		System.out.println(num1 + "x" + ++num2 +"=" +(num1 * num2));
+		System.out.println(num1 + "x" + ++num2 +"=" +(num1 * num2));
+		System.out.println(num1 + "x" + ++num2 +"=" +(num1 * num2));
+		System.out.println("===========================================");
+		
+		num1 = 3; 
+		num2 = 0;
+		System.out.println("===========================================");
+		
+		System.out.println(num1 + "x" + ++num2 +"=" +(num1 * num2));
+		System.out.println(num1 + "x" + ++num2 +"=" +(num1 * num2));
+		System.out.println(num1 + "x" + ++num2 +"=" +(num1 * num2));
+		System.out.println(num1 + "x" + ++num2 +"=" +(num1 * num2));
+		System.out.println(num1 + "x" + ++num2 +"=" +(num1 * num2));
+		System.out.println(num1 + "x" + ++num2 +"=" +(num1 * num2));
+		System.out.println(num1 + "x" + ++num2 +"=" +(num1 * num2));
+		System.out.println(num1 + "x" + ++num2 +"=" +(num1 * num2));
+		System.out.println(num1 + "x" + ++num2 +"=" +(num1 * num2));
+		
+		num1 = 4;
+		num2 = 0;
+		
+		System.out.println("===========================================");
+		System.out.println(num1 + "x" + ++num2 +"=" +(num1 * num2));
+		System.out.println(num1 + "x" + ++num2 +"=" +(num1 * num2));
+		System.out.println(num1 + "x" + ++num2 +"=" +(num1 * num2));
+		System.out.println(num1 + "x" + ++num2 +"=" +(num1 * num2));
+		System.out.println(num1 + "x" + ++num2 +"=" +(num1 * num2));
+		System.out.println(num1 + "x" + ++num2 +"=" +(num1 * num2));
+		System.out.println(num1 + "x" + ++num2 +"=" +(num1 * num2));
+		System.out.println(num1 + "x" + ++num2 +"=" +(num1 * num2));
+		System.out.println(num1 + "x" + ++num2 +"=" +(num1 * num2));
+		System.out.println("===========================================");
+		
+		num1 = 5;
+		num2 = 0;
+		System.out.println("===========================================");
+		System.out.println(num1 + "x" + ++num2 +"=" +(num1 * num2));
+		System.out.println(num1 + "x" + ++num2 +"=" +(num1 * num2));
+		System.out.println(num1 + "x" + ++num2 +"=" +(num1 * num2));
+		System.out.println(num1 + "x" + ++num2 +"=" +(num1 * num2));
+		System.out.println(num1 + "x" + ++num2 +"=" +(num1 * num2));
+		System.out.println(num1 + "x" + ++num2 +"=" +(num1 * num2));
+		System.out.println(num1 + "x" + ++num2 +"=" +(num1 * num2));
+		System.out.println(num1 + "x" + ++num2 +"=" +(num1 * num2));
+		System.out.println(num1 + "x" + ++num2 +"=" +(num1 * num2));
+		System.out.println("===========================================");
+		
+		
+	    num1 = 2;
+	    num2 = 0;
+	    
+		System.out.printf("%d x %d = %d\n" , num1, ++num2,(num1 * num2));
+		System.out.printf("%d x %d = %d\n" , num1, ++num2,(num1 * num2));
+		
+		System.out.printf("%d x %d = %d\n" , num1, ++num2,(num1 * num2));
+
+		System.out.printf("%d x %d = %d\n" , num1, ++num2,(num1 * num2));
+
+		System.out.printf("%d x %d = %d" , num1, ++num2,(num1 * num2));
+	
+		System.out.printf("%d x %d = %d" , num1, ++num2,(num1 * num2));
+	
+		System.out.printf("%d x %d = %d" , num1, ++num2,(num1 * num2));
+	
+		System.out.printf("%d x %d = %d" , num1, ++num2,(num1 * num2));
+	
+		System.out.printf("%d x %d = %d" , num1, ++num2,(num1 * num2));
+	
+		System.out.printf("%d x %d = %d" , num1, ++num2,(num1 * num2));
+	
+	
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
---
+++ Java_05/src/com/biz/hello/Var_02.java
@@ -0,0 +1,110 @@
+package com.biz.hello;
+
+public class Var_02 {
+	
+	public static void main(String[] args) {
+		
+	  int  num1 = 2;
+	  int  num2 = 0;
+	    
+		System.out.printf("%d x %d = %d\n" , num1, ++num2,(num1 * num2));
+		
+		System.out.printf("%d x %d = %d\n" , num1, ++num2,(num1 * num2));
+		
+		System.out.printf("%d x %d = %d\n" , num1, ++num2,(num1 * num2));
+
+		System.out.printf("%d x %d = %d\n" , num1, ++num2,(num1 * num2));
+
+		System.out.printf("%d x %d = %d\n" , num1, ++num2,(num1 * num2));
+	
+		System.out.printf("%d x %d = %d\n" , num1, ++num2,(num1 * num2));
+	
+		System.out.printf("%d x %d = %d\n" , num1, ++num2,(num1 * num2));
+	
+		System.out.printf("%d x %d = %d\n" , num1, ++num2,(num1 * num2));
+	
+		System.out.printf("%d x %d = %d\n" , num1, ++num2,(num1 * num2));
+	
+		System.out.printf("%d x %d = %d\n" , num1, ++num2,(num1 * num2));
+		
+		
+		num1 = 3;
+		num2 = 0;
+        System.out.printf("%d x %d = %d\n" , num1, ++num2,(num1 * num2));
+		
+		System.out.printf("%d x %d = %d\n" , num1, ++num2,(num1 * num2));
+		
+		System.out.printf("%d x %d = %d\n" , num1, ++num2,(num1 * num2));
+
+		System.out.printf("%d x %d = %d\n" , num1, ++num2,(num1 * num2));
+
+		System.out.printf("%d x %d = %d\n" , num1, ++num2,(num1 * num2));
+	
+		System.out.printf("%d x %d = %d\n" , num1, ++num2,(num1 * num2));
+	
+		System.out.printf("%d x %d = %d\n" , num1, ++num2,(num1 * num2));
+	
+		System.out.printf("%d x %d = %d\n" , num1, ++num2,(num1 * num2));
+	
+		System.out.printf("%d x %d = %d\n" , num1, ++num2,(num1 * num2));
+	
+		System.out.printf("%d x %d = %d\n" , num1, ++num2,(num1 * num2));
+		
+		
+		num1 = 4;
+		num2 = 0;
+		 System.out.printf("%d x %d = %d\n" , num1, ++num2,(num1 * num2));
+			
+			System.out.printf("%d x %d = %d\n" , num1, ++num2,(num1 * num2));
+			
+			System.out.printf("%d x %d = %d\n" , num1, ++num2,(num1 * num2));
+
+			System.out.printf("%d x %d = %d\n" , num1, ++num2,(num1 * num2));
+
+			System.out.printf("%d x %d = %d\n" , num1, ++num2,(num1 * num2));
+		
+			System.out.printf("%d x %d = %d\n" , num1, ++num2,(num1 * num2));
+		
+			System.out.printf("%d x %d = %d\n" , num1, ++num2,(num1 * num2));
+		
+			System.out.printf("%d x %d = %d\n" , num1, ++num2,(num1 * num2));
+		
+			System.out.printf("%d x %d = %d\n" , num1, ++num2,(num1 * num2));
+		
+			System.out.printf("%d x %d = %d\n" , num1, ++num2,(num1 * num2));
+			
+			
+			++num1;
+			num2 = 0;
+			
+			 System.out.printf("%d x %d = %d\n" , num1, ++num2,(num1 * num2));
+				
+				System.out.printf("%d x %d = %d\n" , num1, ++num2,(num1 * num2));
+				
+				System.out.printf("%d x %d = %d\n" , num1, ++num2,(num1 * num2));
+
+				System.out.printf("%d x %d = %d\n" , num1, ++num2,(num1 * num2));
+
+				System.out.printf("%d x %d = %d\n" , num1, ++num2,(num1 * num2));
+			
+				System.out.printf("%d x %d = %d\n" , num1, ++num2,(num1 * num2));
+			
+				System.out.printf("%d x %d = %d\n" , num1, ++num2,(num1 * num2));
+			
+				System.out.printf("%d x %d = %d\n" , num1, ++num2,(num1 * num2));
+			
+				System.out.printf("%d x %d = %d\n" , num1, ++num2,(num1 * num2));
+			
+				System.out.printf("%d x %d = %d\n" , num1, ++num2,(num1 * num2));
+				
+				
+				num1 = 7;
+				num2 = 0;
+				
+		
+		
+		
+		
+	}
+
+}
