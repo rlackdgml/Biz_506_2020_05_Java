e46c9c13473737fb591a9de16502a67e36a6068a
rlackdgml <dhdlqkqh2003@naver.com>
2020-05-28 17:47:57 +0900

    객체

---
+++ Java_09/src/com/biz/grade/Grade_03.java
@@ -0,0 +1,18 @@
+package com.biz.grade;
+
+import java.util.Random;
+
+public class Grade_03 {
+
+	public static void main(String[] args) {
+		int num1 = 0;
+
+		Random rnd = new Random();
+		num1 = rnd.nextInt() * 55 ;
+		
+		int num2 = num1 * 55;
+		System.out.println(num2);
+
+	}
+
+}
